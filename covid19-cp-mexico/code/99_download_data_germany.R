d801802656d0e67ff107ba22c5612601cb368cee
nora <nora.ghenciulescu@gmail.com>
2025-05-21 19:51:11 +0200

    changed for readability

--- covid19-cp-mexico/code/99_download_data_germany.R
+++
@@ -1,7 +0,0 @@
-# This file can be used to download the current RKI data
-# We used it on 2021-01-05 to download the current dataset and safed it in the data/raw folder
-file_path <- file.path("..","data/raw/", str_c("RKI-",Sys.Date(),  ".csv"))
-if (!file.exists(file_path)) {   
-  url_RKI_dashboard_data <- "https://www.arcgis.com/sharing/rest/content/items/f10774f1c63e40168479a1feb6c7ca74/data"
-  download.file(url=url_RKI_dashboard_data, destfile=file_path, mode="wb")
-}
